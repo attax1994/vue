79eea204c1d87637db29a1a870857dc33d753f3c
attax1994 <lqytyui@qq.com>
2018-05-24 18:45:31 +0800

    core/observe/index

--- src/core/observer/dep.js
+++ src/core/observer/dep.js
@@ -1,40 +1,43 @@
 /* @flow */
 
 import type Watcher from './watcher'
-import { remove } from '../util/index'
+import {remove} from '../util/index'
 
 let uid = 0
 
 /**
- * A dep is an observable that can have multiple
- * directives subscribing to it.
+ * dep是一个可以让多个指令去订阅它的观察者
  */
 export default class Dep {
-  static target: ?Watcher;
-  id: number;
-  subs: Array<Watcher>;
+  static target: ?Watcher
+  id: number
+  subs: Array<Watcher>
 
-  constructor () {
+  constructor() {
     this.id = uid++
     this.subs = []
   }
 
-  addSub (sub: Watcher) {
+  // 添加一个Watcher
+  addSub(sub: Watcher) {
     this.subs.push(sub)
   }
 
-  removeSub (sub: Watcher) {
+  // 移除一个Watcher
+  removeSub(sub: Watcher) {
     remove(this.subs, sub)
   }
 
-  depend () {
+  // 依赖收集，当存在Dep.target的时候添加观察者对象
+  depend() {
     if (Dep.target) {
       Dep.target.addDep(this)
     }
   }
 
-  notify () {
-    // stabilize the subscriber list first
+  // 通知所有订阅者，发生了update
+  notify() {
+    // 保证subs为Array
     const subs = this.subs.slice()
     for (let i = 0, l = subs.length; i < l; i++) {
       subs[i].update()
@@ -42,17 +45,20 @@ export default class Dep {
   }
 }
 
-// the current target watcher being evaluated.
-// this is globally unique because there could be only one
-// watcher being evaluated at any time.
+/**
+ * Dep.target指示当前正在做检查的watcher。
+ * 它是一个静态属性，在全局是独一无二的，因为同一时间只会有一个watcher在做检查
+ */
 Dep.target = null
 const targetStack = []
 
-export function pushTarget (_target: ?Watcher) {
+// push一个Watcher进入targetStack，Dep.target记录最新的一条记录
+export function pushTarget(_target: ?Watcher) {
   if (Dep.target) targetStack.push(Dep.target)
   Dep.target = _target
 }
 
-export function popTarget () {
+// pop顶层Watcher退出targetStack，提供给Dep.target
+export function popTarget() {
   Dep.target = targetStack.pop()
 }
--- src/core/observer/index.js
+++ src/core/observer/index.js
@@ -2,7 +2,7 @@
 
 import Dep from './dep'
 import VNode from '../vdom/vnode'
-import { arrayMethods } from './array'
+import {arrayMethods} from './array'
 import {
   def,
   warn,
@@ -13,7 +13,7 @@ import {
   isPrimitive,
   isUndef,
   isValidArrayIndex,
-  isServerRendering
+  isServerRendering,
 } from '../util/index'
 
 const arrayKeys = Object.getOwnPropertyNames(arrayMethods)
@@ -24,43 +24,50 @@ const arrayKeys = Object.getOwnPropertyNames(arrayMethods)
  */
 export let shouldObserve: boolean = true
 
-export function toggleObserving (value: boolean) {
+export function toggleObserving(value: boolean) {
   shouldObserve = value
 }
 
 /**
- * Observer class that is attached to each observed
- * object. Once attached, the observer converts the target
- * object's property keys into getter/setters that
- * collect dependencies and dispatch updates.
+ * Observer类，附着到每个被观察的对象。
+ * 一旦被添加，observer会将目标对象的属性转换为getter/setters形式，用于收集依赖和发出更新通知
  */
 export class Observer {
-  value: any;
-  dep: Dep;
-  vmCount: number; // number of vms that has this object as root $data
+  value: any
+  dep: Dep
+  // 将这个Object作为根$data的vm数量
+  vmCount: number
 
-  constructor (value: any) {
+  constructor(value: any) {
     this.value = value
     this.dep = new Dep()
     this.vmCount = 0
+    // 将这个实例作为目标对象的__ob__属性
     def(value, '__ob__', this)
+
     if (Array.isArray(value)) {
+      /**
+       * 如果是数组，对数组的部分原生方法进行修改，从而当它们被调用时，会发出更新的通知。
+       * 如果当前浏览器支持__proto__属性，则直接覆盖当前数组对象原型上的原生数组方法，如果不支持该属性，则直接覆盖数组对象的原型。
+       */
       const augment = hasProto
         ? protoAugment
         : copyAugment
       augment(value, arrayMethods, arrayKeys)
       this.observeArray(value)
     } else {
+      /**
+       * 如果是对象，直接进行遍历绑定
+       */
       this.walk(value)
     }
   }
 
   /**
-   * Walk through each property and convert them into
-   * getter/setters. This method should only be called when
-   * value type is Object.
+   * 遍历每个属性，将他们转换成getter/setter形式。
+   * 这个方法只应在value的类型为Object的时候调用
    */
-  walk (obj: Object) {
+  walk(obj: Object) {
     const keys = Object.keys(obj)
     for (let i = 0; i < keys.length; i++) {
       defineReactive(obj, keys[i])
@@ -68,33 +75,29 @@ export class Observer {
   }
 
   /**
-   * Observe a list of Array items.
+   * 将一个Array的每个item加入观察
    */
-  observeArray (items: Array<any>) {
+  observeArray(items: Array<any>) {
     for (let i = 0, l = items.length; i < l; i++) {
       observe(items[i])
     }
   }
 }
 
+
 // helpers
 
 /**
- * Augment an target Object or Array by intercepting
- * the prototype chain using __proto__
+ * 如果支持__proto__属性，直接将target的__proto__替换为src
  */
-function protoAugment (target, src: Object, keys: any) {
-  /* eslint-disable no-proto */
+function protoAugment(target, src: Object, keys: any) {
   target.__proto__ = src
-  /* eslint-enable no-proto */
 }
 
 /**
- * Augment an target Object or Array by defining
- * hidden properties.
+ * 如果不支持__proto__属性，遍历src的每个键值对，混入到target上去
  */
-/* istanbul ignore next */
-function copyAugment (target: Object, src: Object, keys: Array<string>) {
+function copyAugment(target: Object, src: Object, keys: Array<string>) {
   for (let i = 0, l = keys.length; i < l; i++) {
     const key = keys[i]
     def(target, key, src[key])
@@ -106,7 +109,7 @@ function copyAugment (target: Object, src: Object, keys: Array<string>) {
  * returns the new observer if successfully observed,
  * or the existing observer if the value already has one.
  */
-export function observe (value: any, asRootData: ?boolean): Observer | void {
+export function observe(value: any, asRootData: ?boolean): Observer | void {
   if (!isObject(value) || value instanceof VNode) {
     return
   }
@@ -131,12 +134,12 @@ export function observe (value: any, asRootData: ?boolean): Observer | void {
 /**
  * Define a reactive property on an Object.
  */
-export function defineReactive (
+export function defineReactive(
   obj: Object,
   key: string,
   val: any,
   customSetter?: ?Function,
-  shallow?: boolean
+  shallow?: boolean,
 ) {
   const dep = new Dep()
 
@@ -156,7 +159,7 @@ export function defineReactive (
   Object.defineProperty(obj, key, {
     enumerable: true,
     configurable: true,
-    get: function reactiveGetter () {
+    get: function reactiveGetter() {
       const value = getter ? getter.call(obj) : val
       if (Dep.target) {
         dep.depend()
@@ -169,7 +172,7 @@ export function defineReactive (
       }
       return value
     },
-    set: function reactiveSetter (newVal) {
+    set: function reactiveSetter(newVal) {
       const value = getter ? getter.call(obj) : val
       /* eslint-disable no-self-compare */
       if (newVal === value || (newVal !== newVal && value !== value)) {
@@ -186,7 +189,7 @@ export function defineReactive (
       }
       childOb = !shallow && observe(newVal)
       dep.notify()
-    }
+    },
   })
 }
 
@@ -195,7 +198,7 @@ export function defineReactive (
  * triggers change notification if the property doesn't
  * already exist.
  */
-export function set (target: Array<any> | Object, key: any, val: any): any {
+export function set(target: Array<any> | Object, key: any, val: any): any {
   if (process.env.NODE_ENV !== 'production' &&
     (isUndef(target) || isPrimitive(target))
   ) {
@@ -214,7 +217,7 @@ export function set (target: Array<any> | Object, key: any, val: any): any {
   if (target._isVue || (ob && ob.vmCount)) {
     process.env.NODE_ENV !== 'production' && warn(
       'Avoid adding reactive properties to a Vue instance or its root $data ' +
-      'at runtime - declare it upfront in the data option.'
+      'at runtime - declare it upfront in the data option.',
     )
     return val
   }
@@ -230,7 +233,7 @@ export function set (target: Array<any> | Object, key: any, val: any): any {
 /**
  * Delete a property and trigger change if necessary.
  */
-export function del (target: Array<any> | Object, key: any) {
+export function del(target: Array<any> | Object, key: any) {
   if (process.env.NODE_ENV !== 'production' &&
     (isUndef(target) || isPrimitive(target))
   ) {
@@ -244,7 +247,7 @@ export function del (target: Array<any> | Object, key: any) {
   if (target._isVue || (ob && ob.vmCount)) {
     process.env.NODE_ENV !== 'production' && warn(
       'Avoid deleting properties on a Vue instance or its root $data ' +
-      '- just set it to null.'
+      '- just set it to null.',
     )
     return
   }
@@ -262,7 +265,7 @@ export function del (target: Array<any> | Object, key: any) {
  * Collect dependencies on array elements when the array is touched, since
  * we cannot intercept array element access like property getters.
  */
-function dependArray (value: Array<any>) {
+function dependArray(value: Array<any>) {
   for (let e, i = 0, l = value.length; i < l; i++) {
     e = value[i]
     e && e.__ob__ && e.__ob__.dep.depend()
