da9c8950d315fc2abae5760e0b1e75b2d07a8c3b
attax1994 <lqytyui@qq.com>
2018-05-24 17:10:37 +0800

    core/instance/inject

--- src/core/instance/inject.js
+++ src/core/instance/inject.js
@@ -1,10 +1,15 @@
 /* @flow */
 
-import { hasOwn } from 'shared/util'
-import { warn, hasSymbol } from '../util/index'
-import { defineReactive, toggleObserving } from '../observer/index'
+import {hasOwn} from '../../shared/util'
+import {warn, hasSymbol} from '../util/index'
+import {defineReactive, toggleObserving} from '../observer/index'
 
-export function initProvide (vm: Component) {
+/**
+ * 初始化provide，记录在vm._provided上。
+ * provide可以传入function，需要调用一下获得结果
+ * @param vm
+ */
+export function initProvide(vm: Component) {
   const provide = vm.$options.provide
   if (provide) {
     vm._provided = typeof provide === 'function'
@@ -13,19 +18,24 @@ export function initProvide (vm: Component) {
   }
 }
 
-export function initInjections (vm: Component) {
+/**
+ * 初始化injections
+ * @param vm
+ */
+export function initInjections(vm: Component) {
+  // 寻找依赖
   const result = resolveInject(vm.$options.inject, vm)
   if (result) {
     toggleObserving(false)
+    // 将每个依赖改造为响应式的，并提示不要进行直接的修改
     Object.keys(result).forEach(key => {
-      /* istanbul ignore else */
       if (process.env.NODE_ENV !== 'production') {
         defineReactive(vm, key, result[key], () => {
           warn(
             `Avoid mutating an injected value directly since the changes will be ` +
             `overwritten whenever the provided component re-renders. ` +
             `injection being mutated: "${key}"`,
-            vm
+            vm,
           )
         })
       } else {
@@ -36,21 +46,27 @@ export function initInjections (vm: Component) {
   }
 }
 
-export function resolveInject (inject: any, vm: Component): ?Object {
+/**
+ * 寻找对应的依赖
+ * @param inject
+ * @param vm
+ * @return {any}
+ */
+export function resolveInject(inject: any, vm: Component): ?Object {
   if (inject) {
-    // inject is :any because flow is not smart enough to figure out cached
     const result = Object.create(null)
+    // 过滤出可枚举的属性
     const keys = hasSymbol
-      ? Reflect.ownKeys(inject).filter(key => {
-        /* istanbul ignore next */
-        return Object.getOwnPropertyDescriptor(inject, key).enumerable
-      })
+      ? Reflect.ownKeys(inject).filter(key => Object.getOwnPropertyDescriptor(inject, key).enumerable)
       : Object.keys(inject)
 
     for (let i = 0; i < keys.length; i++) {
       const key = keys[i]
       const provideKey = inject[key].from
       let source = vm
+      /**
+       * 从自身开始，遵循就近原则，逐层向上，在上级组件的_provided属性中寻找对应依赖
+       */
       while (source) {
         if (source._provided && hasOwn(source._provided, provideKey)) {
           result[key] = source._provided[provideKey]
@@ -58,6 +74,11 @@ export function resolveInject (inject: any, vm: Component): ?Object {
         }
         source = source.$parent
       }
+
+      /**
+       * 如果没找到，检查该依赖有没有设置default，如有则使用default
+       * 如果没有default，就要提示没有找到依赖
+       */
       if (!source) {
         if ('default' in inject[key]) {
           const provideDefault = inject[key].default
